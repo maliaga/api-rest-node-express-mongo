b443466159bf7347f406ae20caf437388607755b
Mario Aliaga <ma@marioaliaga.com>
2017-06-21 22:16:56 -0400

    Add test method get

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const port = process.env.PORT || 3001
 app.use(bodyParse.urlencoded({ extended: false}))
 app.use(bodyParse.json())
 
+app.get('/hola/:name', ( req, res) => {
+    res.send({ message: `Hola Mundo ${req.params.name}!`})
+})
 
 app.listen(port, () => {
         console.log(`API REST run in http://localhost:${port}`);
