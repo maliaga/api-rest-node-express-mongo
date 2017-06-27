65a5ff6133722eefaf91705c716173f9307bcc0c
Mario Aliaga <ma@marioaliaga.com>
2017-06-27 12:10:15 -0400

    Refactoring config

---
+++ config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by maliaga on 6/27/17.
+ */
+module.exports = {
+    port : process.env.PORT || 3001,
+    db: process.env.DB || 'mongodb://localhost:27017/shop'
+}
--- index.js
+++ index.js
@@ -5,15 +5,15 @@
 
 const mongoose = require('mongoose')
 var app = require('./app')
-const port = process.env.PORT || 3001;
+const config = require('./config')
 
-mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+mongoose.connect(config.db, (err, res) => {
     if (err) {
         return console.log(`Error en la conección de la base de datos!`)
     }
     console.log('Connection to data base established...')
 
-    app.listen(port, () => {
-        console.log(`API REST run in http://localhost:${port}`)
+    app.listen(config.port, () => {
+        console.log(`API REST run in http://localhost:${config.port}`)
     })
 });
