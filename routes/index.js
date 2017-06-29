b391df7e99ddd68329a4238f76c7944cd574b424
Mario Aliaga <ma@marioaliaga.com>
2017-06-29 11:07:25 -0400

    Add logIn and logOut

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,16 @@
 const express = require('express')
 const api = express.Router()
 const productCtrl = require('../controllers/product')
+const auth = require('../middlewares/auth')
 
 api.get('/product', productCtrl.getProducts);
-api.get('/product/:productId',productCtrl.getProductById);
+api.get('/product/:productId', productCtrl.getProductById);
 api.post('/product', productCtrl.saveProduct);
 api.put('/product/:productId', productCtrl.updateProduct);
 api.delete('/product/:productId', productCtrl.deleteProduct);
+
+api.get('/private', auth, function (req, res) {
+    res.status(200).send({message: `Tienes acceso`})
+})
 
 module.exports = api
--- services/index.js
+++ services/index.js
@@ -14,7 +14,35 @@ function createToken(user) {
         iat: moment.unix(),
         exp: moment.add(14, 'days').unix()
     }
-     return  jwt.encode(payload, config.SECRET_TOKEN)
+    return jwt.encode(payload, config.SECRET_TOKEN)
 }
 
-module.exports = createToken
+function decodeToken(token) {
+    const decoded = new Promise((resolve, reject) => {
+        try {
+            const payload = jwt.decode(token, config.SECRET_TOKEN)
+
+            if (payload <= moment().unix()) {
+                reject({
+                    status: 401,
+                    message: `El token ha expirado`
+                })
+            }
+
+            resolve(payload.sub)
+
+        } catch (err) {
+            reject({
+                status: 500,
+                message: `Invalid Token`
+            })
+        }
+    })
+
+    return decoded
+}
+
+module.exports = {
+    createToken,
+    decodeToken
+}
